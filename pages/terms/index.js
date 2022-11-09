d99f08e1b3b1644f1fc48a9cfdc8ecd3c3a3331a
Aun Shahbaz Awan <60698728+Aun-Shahbaz-Awan@users.noreply.github.com>
2022-11-09 14:27:14 +0500

    update-terms

--- pages/terms/index.js
+++ pages/terms/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BsArrowRight } from "react-icons/bs";
+// import { BsArrowRight } from "react-icons/bs";
 
 function Index() {
   return (
     <div className="text-primary my-12 max-w-4xl mx-auto px-4">
       <h4 className="text-5xl pt-12">Terms of Service</h4>
       <p className="italic mt-2">Last Revised: July 11th 2022</p>
-      <p className="text-gray-700 my-4">
+      <p className="text-gray-700 my-6">
         Please read these Terms of Service (this “Agreement”) carefully. Your
         use or access of the Site or the Services (as defined below) constitutes
         your consent to this Agreement. If you do not agree to this Agreement,
@@ -14,28 +14,63 @@ function Index() {
         Site and the Services. Your continued use of the Site and Services is
         your ongoing acceptance of this Agreement.
       </p>
-      <p className="text-gray-700 my-4">
+      <p className="text-gray-700 my-6">
         This Agreement is between you (the “User” or “You”, and collectively
         with others using the Site, “Users”) and Oazo Apps Limited, a company
         incorporated and registered in England, United Kingdom (“Company” or
         “we,” “our” or “us” and together with you, the “Parties”) concerning
-        your use of (including any access to) Company’s websites, currently
+        your use of (including any access to) Company websites, currently
         located at oasis.app, mobile applications, web applications,
         decentralized applications, smart contracts and API located at any of
-        Company’s websites (together with any materials and services available
+        Company websites (together with any materials and services available
         therein, and successor website(s) or application(s) thereto, the
         “Site”). This Agreement hereby incorporates by this reference any
         additional terms and conditions with respect to the Site posted by
         Company to the Site, or otherwise made available to you by Company.
       </p>
-
+      <p className="text-gray-700 my-6">
+        <strong>IMPORTANT:</strong> This Agreement contains a mandatory
+        individual Arbitration clause and class action and jury trial waivers
+        that require the use of arbitration by Users on an individual basis to
+        resolve Disputes, rather than jury trials or class actions or similar
+        group actions. The Site and Services are only available to you and
+        should only be used by you if you agree to these waivers.
+      </p>
+      <p className="text-gray-700 my-6">
+        By clicking or tapping any button or box marked “accept” or “agree” (or
+        a similar term) in connection with this Agreement, or by accessing or
+        using the Site or the Services (as defined below), you agree to be bound
+        by this Agreement, a current version of which is available at the Site,
+        and which may be modified from time to time at our sole discretion in
+        accordance with section 3 below.
+      </p>
+      <h4 className="text-4xl pt-4">1. Use of Services</h4>
+      <h5 className="text-3xl py-4">1.1. Services</h5>
+      The Company has developed Oasis an online, decentralized and autonomous
+      environment that displays price information and autonomous smart contract
+      mechanisms of digital tokens and assets (“Digital Assets”) and provides
+      the user-interface that guides your access to Oasis; the Site is used to
+      provide Users with access to certain decentralized finance services and
+      products on the Ethereum blockchain through Oasis (the “Services.”) The
+      Services may require users to pay fees, such as gas charges on the
+      Ethereum network, to perform a transaction. You acknowledge and agree that
+      the Company has no control over User transactions, User digital assets,
+      the method or amount of payment required for any User to engage in any
+      transaction, or any payments of fees for any transactions. You must ensure
+      that you have a sufficient balance to complete any transaction using the
+      Site or Services before initiating such transaction. User affirms that the
+      User is aware and acknowledges that Company is a noncustodial provider of
+      software services, meaning that the Company does not custody, control, or
+      manage User funds in any manner whatsoever. The Services are deployed in a
+      decentralized environment wherein the Services can be autonomously and
+      directly accessed by the Users without any involvement or actions taken by
+      Company or any third-party.
       {/* <h5 className="text-2xl font-semibold mb-8 flex items-center hover:gap-1">
         Meet the team
         <span className="ml-2">
           <BsArrowRight />
         </span>
       </h5> */}
-
     </div>
   );
 }
